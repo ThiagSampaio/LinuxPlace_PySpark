853c25a44aa2643af985ee012abc2657d7f0fccb
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-09 15:09:35 -0300

    finaliza a parte inicial do arquivo

--- classes/class_pyspark.py
+++ classes/class_pyspark.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import json
 import os
 import re
@@ -5,3 +7,13 @@ import sys
 from typing import Callable, Optional
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
+
+
+class Sparkclass:
+
+    # referenciando a classe em si.
+    def __init__(self, config: dict):
+        self.config = config
+
+    def sparkStart(self, kwargs: dict) -> SparkSession:
+        print(self.config)
