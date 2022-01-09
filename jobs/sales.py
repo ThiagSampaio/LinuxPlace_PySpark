4d31964633c53fdd1fba7968c06134609b61ffcc
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-09 17:50:11 -0300

    Adiciona logs utilizando pyhton logging

---
+++ jobs/sales.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import logging
+import json
+import os
+import re
+import sys
+from typing import Callable, Optional
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import SparkSession
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_FILE = f"{project_dir}/logs/job-{os.path.basename(__file__)}.log"
+LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
+logging.basicConfig(filename=LOG_FILE, level=logging.debug, format=LOG_FORMAT)
+logger = logging.getLogger('py4j')
