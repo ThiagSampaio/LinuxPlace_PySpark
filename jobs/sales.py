b61adffdfcff42480a3b8a3ffeaec0145e4bcd4e
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-09 17:53:41 -0300

    acerta bug no logger

--- jobs/sales.py
+++ jobs/sales.py
@@ -11,5 +11,8 @@ from pyspark.sql import SparkSession
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_FILE = f"{project_dir}/logs/job-{os.path.basename(__file__)}.log"
 LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
-logging.basicConfig(filename=LOG_FILE, level=logging.debug, format=LOG_FORMAT)
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger('py4j')
+
+
+print(sys.path)
