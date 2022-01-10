2e88ff712b90e122be2c0411791a35732fc25d50
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2022-01-10 02:50:11 -0300

    ajusta sales

--- jobs/sales.py
+++ jobs/sales.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import logging
-import json
-import os
-import re
-import sys
-from typing import Callable, Optional
-from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from typing import Callable, Optional
+import sys
+import re
+import os
+import json
+import logging
 
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_FILE = f"{project_dir}/logs/job-{os.path.basename(__file__)}.log"
@@ -14,5 +14,5 @@ LOG_FORMAT = f"%(asctime)s - LINE:%(lineno)d - %(name)s - %(levelname)s - %(func
 logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG, format=LOG_FORMAT)
 logger = logging.getLogger('py4j')
 
-
-print(sys.path)
+sys.path.insert(1, project_dir)
+from classes import class_pyspark
